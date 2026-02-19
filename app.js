abc1723e5092a6060677090453df9fe414c4db73
Goklas Irvan Joshua Harianja <goklasirvanjoshuaharianja@gmail.com>
2026-02-19 03:15:08 -0500

    fix start script

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ app.use("/", indexRouter);
 app.use("/anggota", membersRouter);
 app.use("/gallery", galleryRouter);
 
-// Server
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
